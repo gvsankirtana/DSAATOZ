726a1076b9770f00a890e51c3710f338bd5a56c0
Guntuboina Sankirtana <59257797+gvsankirtana@users.noreply.github.com>
2026-07-20 21:13:24 +0530

    Create insertion sort.java

---
+++ sorting/insertion sort.java
@@ -0,0 +1,16 @@
+class Solution {
+  //TC-->O(N^2) worst and avg best case --> O(N)
+    public int[] insertionSort(int[] nums) {
+       int n = nums.length;
+       for(int i=0;i<=n-1;i++){
+        int j = i;
+        while(j>0&&nums[j-1]>nums[j]){
+            int temp = nums[j-1];
+            nums[j-1]=nums[j];
+            nums[j]=temp;
+            j--;
+        }
+       }
+       return nums;
+    }
+}
