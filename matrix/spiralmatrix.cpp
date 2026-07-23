747025a849870f62999852cd4b608eca91beec04
Guntuboina Sankirtana <59257797+gvsankirtana@users.noreply.github.com>
2026-07-23 13:18:38 +0530

    Create spiralmatrix.cpp

---
+++ matrix/spiralmatrix.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int left = 0, right = col - 1, top = 0, bottom = row - 1;
+        vector<int> ans;
+        
+        while (left <= right && top <= bottom) {
+            // top row, left to right
+            for (int i = left; i <= right; i++) {
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            
+            // right column, top to bottom
+            for (int i = top; i <= bottom; i++) {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            
+            if (top <= bottom) {   // check if a bottom row still remains
+                for (int i = right; i >= left; i--) {
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            
+            if (left <= right) {   // check if a left column still remains
+                for (int i = bottom; i >= top; i--) {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+};
