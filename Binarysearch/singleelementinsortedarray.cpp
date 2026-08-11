fa480552adc2cae09e9c3d63c7c6435b920d9711
Guntuboina Sankirtana <59257797+gvsankirtana@users.noreply.github.com>
2026-08-11 20:48:59 +0530

    Create singleelementinsortedarray.cpp

---
+++ Binarysearch/singleelementinsortedarray.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        //bruteforce O(N) check left and right 
+        //optimised solution using binary search O(logn)
+        int n = nums.size();
+        if(n==1)return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        int low = 1,high = n-2;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if((nums[mid]!=nums[mid+1])&&(nums[mid]!=nums[mid-1])){
+                return nums[mid];
+            }
+            //we are in left
+            if(((mid%2==1)&&(nums[mid-1]==nums[mid]))||((mid%2==0)&&(nums[mid]==nums[mid+1]))){
+                low = mid+1; //eliminate left
+            }
+            else{
+                high = mid-1; //eliminate right
+            }
+        }
+       return -1;
+    }
+};
